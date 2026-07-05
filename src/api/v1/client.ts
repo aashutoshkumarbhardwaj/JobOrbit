a7ca799b57e8610a5f202c8a7bfdf71320a3e55d
Aashutosh kumar bhardwaj <ashutoshkumarbhardwaj7@gmail.com>
2026-07-05 22:34:21 +0530

    addddddded

--- src/api/v1/client.ts
+++ src/api/v1/client.ts
@@ -125,9 +125,9 @@ class APIClient {
     }
 
     // Add CSRF token for state-changing requests
-    if (this.csrfToken) {
-      headers['X-CSRF-Token'] = this.csrfToken
-    }
+    // if (this.csrfToken) {
+    //   headers['X-CSRF-Token'] = this.csrfToken
+    // }
 
     // Get token from AuthManager (async)
     const token = await this.getAuthToken()
@@ -188,6 +188,8 @@ private buildUrl(
 
   return url.toString()
 }
+
+
 
   /**
    * Handle rate limiting headers
